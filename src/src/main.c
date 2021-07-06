89db05664c0d76847e7e4283ad2b2b089ca558e5
Erhan Kurubas <erhankurubas@gmail.com>
2021-07-07 01:01:46 +0300

    bare metal led on off

--- src/src/main.c
+++ src/src/main.c
@@ -1,64 +1,80 @@
-#include <stdlib.h>
-#include "stm32f10x.h"
+#include <stdint.h>
 
-static volatile uint32_t s_tick_count = 0;
+#define PERIPH_BASE           ((uint32_t)0x40000000) /*!< Peripheral base address in the alias region */
+#define APB1PERIPH_BASE       PERIPH_BASE
+#define APB2PERIPH_BASE       (PERIPH_BASE + 0x10000)
+#define AHBPERIPH_BASE        (PERIPH_BASE + 0x20000)
 
-#define LED_PIN         GPIO_Pin_13
+#define RCC_BASE              (AHBPERIPH_BASE + 0x1000)
+#define GPIOC_BASE            (APB2PERIPH_BASE + 0x1000)
 
-void SysTick_Handler(void)
+#define IOP_LED_PORT          GPIOC
+#define IOP_LED_PIN           13
+
+#define __IO    volatile       /*!< Defines 'read / write' permissions */
+
+typedef struct {
+    __IO uint32_t CR;
+    __IO uint32_t CFGR;
+    __IO uint32_t CIR;
+    __IO uint32_t APB2RSTR;
+    __IO uint32_t APB1RSTR;
+    __IO uint32_t AHBENR;
+    __IO uint32_t APB2ENR;
+    __IO uint32_t APB1ENR;
+    __IO uint32_t BDCR;
+    __IO uint32_t CSR;
+} RCC_TypeDef;
+
+typedef struct {
+    __IO uint32_t CRL;
+    __IO uint32_t CRH;
+    __IO uint32_t IDR;
+    __IO uint32_t ODR;
+    __IO uint32_t BSRR;
+    __IO uint32_t BRR;
+    __IO uint32_t LCKR;
+} GPIO_TypeDef;
+
+#define RCC_APB2ENR_IOPCEN      ((uint32_t)0x00000010)         /*!< I/O port C clock enable */
+#define GPIOC                   ((GPIO_TypeDef *)GPIOC_BASE)
+#define RCC                     ((RCC_TypeDef *)RCC_BASE)
+
+void Delay(void)
 {
-    s_tick_count++;
-}
-
-static void led_init(void)
-{
-    RCC_APB2PeriphClockCmd(RCC_APB2Periph_GPIOC, ENABLE);
-    GPIO_InitTypeDef gpio_out = {
-        .GPIO_Pin = LED_PIN,
-        .GPIO_Mode = GPIO_Mode_Out_PP,
-        .GPIO_Speed = GPIO_Speed_2MHz,
-    };
-    GPIO_Init(GPIOC, &gpio_out);
-}
-
-static void clock_init(void)
-{
-    /* Reset the RCC clock configuration to the default reset state. */
-    /* HSI ON, PLL OFF, HSE OFF, system clock = 72 MHz, cpu_clock = 72 MHz */
-    RCC_DeInit();
-    SystemCoreClockUpdate();  /* BluePill board runs at 72 MHz */
-
-    if (SysTick_Config(SystemCoreClock / 1000)) {
-        /* Capture error */
-        while (1);
-    }
-}
-
-static void delay_us(int32_t us)
-{
-    // Each loop takes very roughly one microsecond on a Blue Pill.
-    volatile int32_t count = us;
-    for (; count > 0; --count)
+    volatile int i = 0;
+    for (i = 0; i < 1000000; i++)
         ;
 }
 
-void delay_ms(uint32_t ms)
+int main()
 {
-    uint32_t end_tick = s_tick_count + ms;
-    while (s_tick_count < end_tick) {
-        __WFE();
-    }
-}
+    int bitOffset;
 
-int main(void)
-{
-    clock_init();
-    led_init();
+    RCC->APB2ENR |= RCC_APB2ENR_IOPCEN;   // GPIOC clock enable
+
+    if (IOP_LED_PIN < 8) {
+        bitOffset = (IOP_LED_PIN - 8) * 4;  // GPIOC->CRH register'inda C13'e karsilik gelen bit konumu
+        GPIOC->CRL &= ~(0xF << bitOffset);
+        GPIOC->CRL |= (3 << bitOffset);
+    } else {
+        bitOffset = (IOP_LED_PIN - 8) * 4;
+        GPIOC->CRH &= ~(0xF << bitOffset);   // GPIOC->CRH<23:20> &= 0000       ..000111100... 11110000111
+        GPIOC->CRH |= (3 << bitOffset);      // GPIOC->CRH<23:20> |= 0011      // *(uint32_t *)0x40011004 = ...
+    }
+
+    GPIOC->ODR |= (1 << IOP_LED_PIN);
+    GPIOC->ODR &= ~(1 << IOP_LED_PIN);
+    GPIOC->ODR |= (1 << IOP_LED_PIN);
+    GPIOC->ODR &= ~(1 << IOP_LED_PIN);
+
+    GPIOC->BSRR = (1 << (IOP_LED_PIN + 16));    // DR<13> = 1
+    GPIOC->BSRR = (1 << IOP_LED_PIN);           // DR<13> = 1
+    GPIOC->BRR = (1 << IOP_LED_PIN);            // DR<13> = 1
+    GPIOC->BSRR = (1 << IOP_LED_PIN);           // DR<13> = 1
 
     while (1) {
-        GPIO_WriteBit(GPIOC, GPIO_Pin_13, Bit_RESET);
-        delay_ms(250);
-        GPIO_WriteBit(GPIOC, GPIO_Pin_13, Bit_SET);
-        delay_us(500000);
+        GPIOC->ODR ^= (1 << IOP_LED_PIN);
+        Delay();
     }
 }
